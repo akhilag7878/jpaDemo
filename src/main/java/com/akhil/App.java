c8ffacb624964e58c375dd731765dd31809f5380
akhill <akhil8120110218@gmail.com>
2025-01-29 23:16:46 +0530

    first commit

---
+++ src/main/java/com/akhil/App.java
@@ -0,0 +1,32 @@
+package com.akhil;
+
+import jakarta.persistence.EntityManager;
+//import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.PersistenceUnit;
+import org.hibernate.sql.ast.tree.expression.AliasedExpression;
+
+/**
+ * Hello world!
+ */
+public class App {
+    @PersistenceUnit(name = "pu")
+   // static EntityManagerFactory emf;
+    public static void main(String[] args) {
+        Alien akhil=new Alien();
+        akhil.setAid(7);
+        akhil.setTech("Hardware");
+        akhil.setAname("akhil");
+      //emf =
+        EntityManager em=Persistence.createEntityManagerFactory("pu")
+                .createEntityManager();
+       // = emf.createEntityManager();
+        em.getTransaction().begin();
+        akhil.setAid(12);
+        em.persist(akhil);
+        akhil.setAname("chitranshu");
+        akhil=em.find(Alien.class,8);
+        em.getTransaction().commit();
+        System.out.println(akhil);
+    }
+}
