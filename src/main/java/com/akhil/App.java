220920b20d0f6d3aaa321ed5cb0c1ff6ec4031da
akhill <akhil8120110218@gmail.com>
2025-02-12 23:10:39 +0530

    Create query in JPA

--- src/main/java/com/akhil/App.java
+++ src/main/java/com/akhil/App.java
@@ -22,10 +22,11 @@ public class App {
                 .createEntityManager();
        // = emf.createEntityManager();
         em.getTransaction().begin();
-        akhil.setAid(12);
-        em.persist(akhil);
-        akhil.setAname("chitranshu");
+        //akhil.setAid(12);
+        //em.persist(akhil);
+        //akhil.setAname("chitranshu");
         akhil=em.find(Alien.class,8);
+       // em.createQuery("").getSingleResult();
         em.getTransaction().commit();
         System.out.println(akhil);
     }
